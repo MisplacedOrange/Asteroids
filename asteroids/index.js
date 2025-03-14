444ebd468e1c8f97b2b07ee2798e8bb126fef36f
MisplacedOrange <Roylu0404@gmail.com>
2025-03-14 16:34:23 -0400

    fixes

--- asteroids/index.js
+++ asteroids/index.js
@@ -1,116 +1,106 @@
-const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 class Player {
     constructor({position, velocity}) {
-        this.position = position // {x, y}
-        this.velocity = velocity
-        this.rotation = 0
+        this.position = position;
+        this.velocity = velocity;
+        this.rotation = 0;
     }
 
     draw() {
-        context.save()
+        context.save();
+        context.translate(this.position.x, this.position.y);
+        context.rotate(this.rotation);
+        context.translate(-this.position.x, -this.position.y);
 
-        context.translate(this.position.x, this.position.y)
-        context.rotate(this.rotation)
-        context.translate(-this.position.x, -this.position.y)
-        
-        context.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false)
-        context.fillStyle = 'white'
-        context.fill()
+        context.beginPath();  // Added this line
+        context.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+        context.fillStyle = 'white';
+        context.fill();
 
-        // context.fillStyle = 'red'
-        // context.fillRect(this.position.x, this.postition.y, 100, 100)
-        context.beginPath()
-        context.moveTo(this.position.x + 30, this.position.y)
-        context.lineTo(this.position.x - 10, this.position.y - 10)
-        context.lineTo(this.position.x - 10, this.position.y + 10)
-        context.closePath()
+        context.beginPath();
+        context.moveTo(this.position.x + 30, this.position.y);
+        context.lineTo(this.position.x - 10, this.position.y - 10);
+        context.lineTo(this.position.x - 10, this.position.y + 10);
+        context.closePath();
 
-        context.strokeStyle = 'white'
-        context.stroke()
-        context.restore()
+        context.strokeStyle = 'white';
+        context.stroke();
+        context.restore();
     }
 
     update() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 }
 
 const player = new Player({
-    position:{x: canvas.width / 2, y: canvas.height / 2},
-    velocity: { x: 0, y: 0},
-})
+    position: { x: canvas.width / 2, y: canvas.height / 2 },
+    velocity: { x: 0, y: 0 },
+});
 
-player.draw
+// Start the animation loop
+animate();
 
 const keys = {
-    w: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    },
-}
+    w: { pressed: false },
+    a: { pressed: false },
+    d: { pressed: false },
+};
 
-const SPEED = 3
-const ROTATIONAL_SPEED = 0.05
-const FRICTION = 0.97
+const SPEED = 3;
+const ROTATIONAL_SPEED = 0.05;
+const FRICTION = 0.97;
 
 function animate() {
-    window.requestAnimationFrame(animate)
-    context.fillStyle = 'black'
-    context.fillRect(0, 0, canvas.width, canvas.height)
+    window.requestAnimationFrame(animate);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    player.update()
+    player.update();
 
     if (keys.w.pressed) {
-        player.velocity.x = Math.cos(player.rotation) * 3
-        player.velocity.y = Math.sin(player.rotation) * 3
-    } else if (!keys.w.pressed) {
-        player.velocity.x *= FRICTION
-        player.velocity.y *= FRICTION
-
+        player.velocity.x = Math.cos(player.rotation) * SPEED;
+        player.velocity.y = Math.sin(player.rotation) * SPEED;
+    } else {
+        player.velocity.x *= FRICTION;
+        player.velocity.y *= FRICTION;
     }
 
-    if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED
-        else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED
+    if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED;
+    else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED;
 }
-
 
 window.addEventListener('keydown', (event) => {
     switch (event.code) {
         case 'KeyW':
-            keys.w.pressed = true
-            break
+            keys.w.pressed = true;
+            break;
         case 'KeyA':
-            keys.a.pressed = true
-            break
+            keys.a.pressed = true;
+            break;
         case 'KeyD':
-            keys.d.pressed = true
-            break
+            keys.d.pressed = true;
+            break;
     }
-})
+});
 
 window.addEventListener('keyup', (event) => {
     switch (event.code) {
         case 'KeyW':
-            keys.w.pressed = false
-            break
+            keys.w.pressed = false;
+            break;
         case 'KeyA':
-            keys.a.pressed = false
-            break
+            keys.a.pressed = false;
+            break;
         case 'KeyD':
-            keys.d.pressed = false
-            break
+            keys.d.pressed = false;
+            break;
     }
-})
+});
